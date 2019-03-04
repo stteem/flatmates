be916c3da3f283253d0ebb96446438c6de1012aa
Uwem Uke <slyart147@gmail.com>
2019-03-04 18:50:08 +0100

    added gtag.js for google analytics

--- build/register.php
+++ build/register.php
@@ -119,6 +119,17 @@ if (isset($_POST['register'])) {
 <!DOCTYPE html>
 <html>
 <head>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135472679-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-135472679-1');
+	</script>
+	
 	<meta charset="UTF-8" >
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
